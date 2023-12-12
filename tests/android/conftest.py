98219ca95ef30ae1db6fd1db08caaa41d11e0cfc
Y3ll0w <k.varbarkin@gmail.com>
2023-12-13 02:10:31 +0300

    Added test for Ios and Android

---
+++ tests/android/conftest.py
@@ -0,0 +1,49 @@
+import pytest
+from appium.options.android import UiAutomator2Options
+from selene import browser
+from dotenv import load_dotenv
+import os
+
+from selenium import webdriver
+
+
+@pytest.fixture(scope='session', autouse=True)
+def load_env():
+    load_dotenv()
+
+
+@pytest.fixture(scope='function', autouse=True)
+def mobile_management():
+    username = os.getenv('USER_NAME')
+    access_key = os.getenv('ACCESS_KEY')
+    remote_browser_url = os.getenv('REMOTE_BROWSER_URL')
+
+    options = UiAutomator2Options().load_capabilities({
+        # Specify device and os_version for testing
+        "platformName": "android",
+        "platformVersion": "13.0",
+        "deviceName": "Samsung Galaxy S23 Ultra",
+
+        # Set URL of the application under test
+        "app": "bs://sample.app",
+
+        # Set other BrowserStack capabilities
+        'bstack:options': {
+            "projectName": "Wikiperdia tests",
+            "buildName": "browserstack-wikipedia-build",
+            "sessionName": "BStack wikipedia_test",
+
+            # Set your access credentials
+            "userName": username,
+            "accessKey": access_key
+        }
+    })
+
+    # browser.config.driver = webdriver.Remote("http://hub.browserstack.com/wd/hub", options=options)
+    browser.config.driver_remote_url = remote_browser_url
+    browser.config.driver_options = options
+    browser.config.timeout = 10.0
+
+    yield
+
+    browser.quit()
---
+++ tests/android/test_wikipedia_open_article.py
@@ -0,0 +1,16 @@
+from allure_commons._allure import step
+from appium.webdriver.common.appiumby import AppiumBy
+from selene import browser, have
+
+
+def test_open_article():
+    # WHEN
+    with step('Search article'):
+        browser.element((AppiumBy.ACCESSIBILITY_ID, "Search Wikipedia")).click()
+        browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type('Appium')
+        browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/page_list_item_title")).click()
+
+    # THEN
+    with step('Verify article found'):
+        results = browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/pcs"))
+        results.should(have.text('Appium'))
